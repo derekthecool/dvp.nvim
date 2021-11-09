8987d1d346cf078af5fdc8ac56b538d9deea98f2
derekthecool <derekthecool@gmail.com>
2021-11-09 10:36:38 -0700

    FT autocmds worked after updating nvim and all plugins, removing comments for them

--- lua/dvp/init.lua
+++ lua/dvp/init.lua
@@ -1,7 +1,5 @@
 local comma_count = require('dvp.comma_count')
 local bit_flip = require('dvp.bit_flip')
--- vim.cmd "au BufNewFile,BufRead *errlogpool*.txt set filetype=errlogpool"
--- vim.cmd "au BufNewFile,BufRead BelleLTE_DataLog*.txt set filetype=mcn_device_logs"
 
 -- Return multiple functions like in this example
 -- https://www.2n.pl/blog/how-to-write-neovim-plugins-in-lua
