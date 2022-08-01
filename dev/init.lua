be4c7eb7e774ec77681a3b5de6e963f18ecb60e6
derekthecool <derekthecool@gmail.com>
2022-07-31 21:08:14 -0600

    Format dev/init.lua

--- dev/init.lua
+++ dev/init.lua
@@ -3,31 +3,31 @@
 --]]
 local package_name = 'dvp'
 
-local escape_pattern = function (text)
-    return text:gsub("([^%w])", "%%%1")
+local escape_pattern = function(text)
+  return text:gsub("([^%w])", "%%%1")
 end
 
 -- unload loaded modules by the matching text
-local unload_packages = function ()
-	local esc_package_name = escape_pattern(package_name)
+local unload_packages = function()
+  local esc_package_name = escape_pattern(package_name)
 
-	for module_name, _ in pairs(package.loaded) do
-		if string.find(module_name, esc_package_name) then
-			package.loaded[module_name] = nil
-		end
-	end
+  for module_name, _ in pairs(package.loaded) do
+    if string.find(module_name, esc_package_name) then
+      package.loaded[module_name] = nil
+    end
+  end
 end
 
 -- executes the run method in the package
-local run_action = function ()
-	require(package_name).comma_count()
-	require(package_name).bit_flip()
+local run_action = function()
+  require(package_name).comma_count()
+  require(package_name).bit_flip()
 end
 
 -- unload and run the function from the package
 function Reload_and_run()
-	unload_packages()
-	run_action()
+  unload_packages()
+  run_action()
 end
 
 local set_keymap = vim.api.nvim_set_keymap
